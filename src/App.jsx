80666393ed612612d67727e12fa4d1332db801d9
kleber-matos <94052079+kleber-matos@users.noreply.github.com>
2024-06-05 11:58:13 -0300

    :sparkles: feat: busca por cep

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import * as S from "./styled.jsx";
+import lupa from "./assets/lupa.png";
 
 export default function App() {
   return (
     <>
-      <h1>busca cep</h1>
+      <S.Container>
+        <S.Box>
+          <h1>BUSCA CEP</h1>
+          <p>Consulte abaixo</p>
+
+          <S.Consulta>
+            <S.Input
+              tamanho="70%"
+              placeholder="Digite seu cep..."
+              type="number"
+            />
+            <S.Button>
+              <img src={lupa} alt="Lupa de pesquisa" />
+            </S.Button>
+          </S.Consulta>
+
+          <S.Forms>
+            <label>Cidade</label>
+            <S.Input tamanho="100%" type="text" />
+            <label>Bairro</label>
+            <S.Input tamanho="100%" type="text" />
+            <label>Logradouro</label>
+            <S.Input tamanho="100%" type="text" />
+            <label>Complemento</label>
+            <S.Input tamanho="100%" type="text" />
+          </S.Forms>
+          <h2>Cep:</h2>
+          <p>123.123-123</p>
+        </S.Box>
+      </S.Container>
     </>
   );
 }
--- src/styled.jsx
+++ src/styled.jsx
@@ -1,11 +1,69 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyled = createGlobalStyle`
-
 * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background-color:#FCD808;
+    font-family: "Poppins", sans-serif;
 }
+`;
 
+export const Container = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+`;
+
+export const Box = styled.section`
+  width: 80%;
+  height: 50vh;
+  max-width: 500px;
+  h1 {
+    font-size: 2rem;
+  }
+`;
+
+export const Consulta = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  background-color: white;
+  border: none;
+  width: ${(props) => props.tamanho};
+  outline: none;
+  height: 4vh;
+  padding-left: 5px;
+  border-radius: 5px;
+  font-size: 1rem;
+  /* Serve para tirar as setas do input */
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+  }
+`;
+
+export const Button = styled.button`
+  width: 10%;
+  height: 4vh;
+  border: none;
+  cursor: pointer;
+  img {
+    height: 3vh;
+    width: 60%;
+  }
+`;
+
+export const Forms = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  height: 30vh;
+  label {
+    display: block;
+  }
 `;
